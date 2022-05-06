7fffccf10c4e2e6b68d2e7d982284a2df6026002
Salekin-Nabil <salekinnabil@gmail.com>
2022-05-07 00:59:20 +0600

    fixed responsiveness

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import banner from '../../images/banner.jpg';
 import './Banner.css';
 
 const Banner = () => {
     return (
         <div className='mb-[3vw]'>
-            <div>
-            <img
-                        src="https://barbend.com/wp-content/uploads/2021/03/The-Best-Arm-Workouts.jpg"
-                        className="w-full md:h-[650px]"
+            <div className="carousel-inner relative w-full overflow-hidden">
+                <div className="carousel-item active relative float-left w-full">
+                    <img
+                        src={banner}
+                        className="block w-full md:h-[650px]"
                         alt="..."
                     />
+                    <div className="carousel-caption md:block absolute text-center">
+                        <h5 className="text-xl">Welcome to Gymniac</h5>
+                        <p>The most complete gym store.</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
